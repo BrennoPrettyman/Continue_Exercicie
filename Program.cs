755ab50d837e644419dd3e31bdec65b90ab43d61
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-18 14:20:36 -0300

    Bem easy

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Continue_Exercicie;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Pressione uma tecla para continuar...");
+        Console.ReadKey();
+
+        Console.Clear();
+        Console.WriteLine("Okay, muito obrigado!! ;-D");
+    }
+}
